4aa07f0824f3e1b6ec7098e5469f8abf6c34da2f
Bill Bartlett <Bill Bartlett>
2016-09-14 03:28:58 -0700

    adding warm_bucket_backup.sh that's referenced in aws deployment guide

---
+++ scripts/warm_bucket_backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#- Splunk variables
+splunk_db=/opt/splunk
+warm_directory=$splunk_db/my_index/db
+
+#- target s3 bucket for backups
+s3_backup_bucket=backup-bucket-name
+
+#- assuming s3 infrequent access storage class
+aws s3 sync $warm_directory/db_* s3://$s3_backup_bucket --storage-class STANDARD_IA
